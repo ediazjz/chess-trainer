f1eceb90fe64b6ea5527878cd3bf437af520b95b
ediazjz <iam.ediazjz@gmail.com>
2024-03-08 09:48:13 -0600

    build: initial commit

---
+++ src/board.ts
@@ -0,0 +1,395 @@
+type TSquare = {
+  name: string,
+  x: number,
+  y: number,
+  color: string
+}
+
+type TBoard = TSquare[]
+
+export const board: TBoard = [
+  {
+    name: 'a1',
+    x: 1,
+    y: 1,
+    color: 'black'
+  },
+  {
+    name: 'a2',
+    x: 1,
+    y: 2,
+    color: 'white'
+  },
+  {
+    name: 'a3',
+    x: 1,
+    y: 3,
+    color: 'black'
+  },
+  {
+    name: 'a4',
+    x: 1,
+    y: 4,
+    color: 'white'
+  },
+  {
+    name: 'a5',
+    x: 1,
+    y: 5,
+    color: 'black'
+  },
+  {
+    name: 'a6',
+    x: 1,
+    y: 6,
+    color: 'white'
+  },
+  {
+    name: 'a7',
+    x: 1,
+    y: 7,
+    color: 'black'
+  },
+  {
+    name: 'a8',
+    x: 1,
+    y: 8,
+    color: 'white'
+  },
+  {
+    name: 'b1',
+    x: 2,
+    y: 1,
+    color: 'black'
+  },
+  {
+    name: 'b2',
+    x: 2,
+    y: 2,
+    color: 'white'
+  },
+  {
+    name: 'b3',
+    x: 2,
+    y: 3,
+    color: 'black'
+  },
+  {
+    name: 'b4',
+    x: 2,
+    y: 4,
+    color: 'white'
+  },
+  {
+    name: 'b5',
+    x: 2,
+    y: 5,
+    color: 'black'
+  },
+  {
+    name: 'b6',
+    x: 2,
+    y: 6,
+    color: 'white'
+  },
+  {
+    name: 'b7',
+    x: 2,
+    y: 7,
+    color: 'black'
+  },
+  {
+    name: 'b8',
+    x: 2,
+    y: 8,
+    color: 'white'
+  },
+  {
+    name: 'c1',
+    x: 3,
+    y: 1,
+    color: 'black'
+  },
+  {
+    name: 'c2',
+    x: 3,
+    y: 2,
+    color: 'white'
+  },
+  {
+    name: 'c3',
+    x: 3,
+    y: 3,
+    color: 'black'
+  },
+  {
+    name: 'c4',
+    x: 3,
+    y: 4,
+    color: 'white'
+  },
+  {
+    name: 'c5',
+    x: 3,
+    y: 5,
+    color: 'black'
+  },
+  {
+    name: 'c6',
+    x: 3,
+    y: 6,
+    color: 'white'
+  },
+  {
+    name: 'c7',
+    x: 3,
+    y: 7,
+    color: 'black'
+  },
+  {
+    name: 'c8',
+    x: 3,
+    y: 8,
+    color: 'white'
+  },
+  {
+    name: 'd1',
+    x: 4,
+    y: 1,
+    color: 'black'
+  },
+  {
+    name: 'd2',
+    x: 4,
+    y: 2,
+    color: 'white'
+  },
+  {
+    name: 'd3',
+    x: 4,
+    y: 3,
+    color: 'black'
+  },
+  {
+    name: 'd4',
+    x: 4,
+    y: 4,
+    color: 'white'
+  },
+  {
+    name: 'd5',
+    x: 4,
+    y: 5,
+    color: 'black'
+  },
+  {
+    name: 'd6',
+    x: 4,
+    y: 6,
+    color: 'white'
+  },
+  {
+    name: 'd7',
+    x: 4,
+    y: 7,
+    color: 'black'
+  },
+  {
+    name: 'd8',
+    x: 4,
+    y: 8,
+    color: 'white'
+  },
+  {
+    name: 'e1',
+    x: 5,
+    y: 1,
+    color: 'black'
+  },
+  {
+    name: 'e2',
+    x: 5,
+    y: 2,
+    color: 'white'
+  },
+  {
+    name: 'e3',
+    x: 5,
+    y: 3,
+    color: 'black'
+  },
+  {
+    name: 'e4',
+    x: 5,
+    y: 4,
+    color: 'white'
+  },
+  {
+    name: 'e5',
+    x: 5,
+    y: 5,
+    color: 'black'
+  },
+  {
+    name: 'e6',
+    x: 5,
+    y: 6,
+    color: 'white'
+  },
+  {
+    name: 'e7',
+    x: 5,
+    y: 7,
+    color: 'black'
+  },
+  {
+    name: 'e8',
+    x: 5,
+    y: 8,
+    color: 'white'
+  },
+  {
+    name: 'f1',
+    x: 6,
+    y: 1,
+    color: 'black'
+  },
+  {
+    name: 'f2',
+    x: 6,
+    y: 2,
+    color: 'white'
+  },
+  {
+    name: 'f3',
+    x: 6,
+    y: 3,
+    color: 'black'
+  },
+  {
+    name: 'f4',
+    x: 6,
+    y: 4,
+    color: 'white'
+  },
+  {
+    name: 'f5',
+    x: 6,
+    y: 5,
+    color: 'black'
+  },
+  {
+    name: 'f6',
+    x: 6,
+    y: 6,
+    color: 'white'
+  },
+  {
+    name: 'f7',
+    x: 6,
+    y: 7,
+    color: 'black'
+  },
+  {
+    name: 'f8',
+    x: 6,
+    y: 8,
+    color: 'white'
+  },
+  {
+    name: 'g1',
+    x: 7,
+    y: 1,
+    color: 'black'
+  },
+  {
+    name: 'g2',
+    x: 7,
+    y: 2,
+    color: 'white'
+  },
+  {
+    name: 'g3',
+    x: 7,
+    y: 3,
+    color: 'black'
+  },
+  {
+    name: 'g4',
+    x: 7,
+    y: 4,
+    color: 'white'
+  },
+  {
+    name: 'g5',
+    x: 7,
+    y: 5,
+    color: 'black'
+  },
+  {
+    name: 'g6',
+    x: 7,
+    y: 6,
+    color: 'white'
+  },
+  {
+    name: 'g7',
+    x: 7,
+    y: 7,
+    color: 'black'
+  },
+  {
+    name: 'g8',
+    x: 7,
+    y: 8,
+    color: 'white'
+  },
+  {
+    name: 'h1',
+    x: 8,
+    y: 1,
+    color: 'black'
+  },
+  {
+    name: 'h2',
+    x: 8,
+    y: 2,
+    color: 'white'
+  },
+  {
+    name: 'h3',
+    x: 8,
+    y: 3,
+    color: 'black'
+  },
+  {
+    name: 'h4',
+    x: 8,
+    y: 4,
+    color: 'white'
+  },
+  {
+    name: 'h5',
+    x: 8,
+    y: 5,
+    color: 'black'
+  },
+  {
+    name: 'h6',
+    x: 8,
+    y: 6,
+    color: 'white'
+  },
+  {
+    name: 'h7',
+    x: 8,
+    y: 7,
+    color: 'black'
+  },
+  {
+    name: 'h8',
+    x: 8,
+    y: 8,
+    color: 'white'
+  },
+]
